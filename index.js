4e8bb5e0951a18f3bc97786fcc622e43acd2144a
Kuzmin Anton <akuzmin.exceedteam@gmail.com>
2021-10-04 11:39:58 +0300

    BE 3 - card (registration)

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const app = express();
-// const apiRoutes = require('./src/modules/routes/routes');
+const apiRoutes = require('./src/modules/routes/routes');
+
 const mongoUserName = 'user';
 const mongoUserPass = 'user';
 const PORT = 5000;
 
+app.use(cors());
 const uri = `mongodb+srv://${mongoUserName}:${mongoUserPass}@cluster0.hihcv.mongodb.net/hospital?retryWrites=true&w=majority`;
 
 mongoose.connect(uri);
 
-app.use(express.json());
+app.use(express.json({ extended: true }));
+app.use('/', apiRoutes);
 app.listen(PORT, () => {
   console.log(`on port ${PORT}`);
 });
--- src/modules/routes/routes.js
+++ src/modules/routes/routes.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
-const {} = require('../controllers/user.cotroller');
+const {
+  newUser,
+} = require('../controllers/user.cotroller');
 const {} = require('../controllers/appointment.controller');
+
+router.post('/auth/newUser', newUser);
 
 module.exports = router;
