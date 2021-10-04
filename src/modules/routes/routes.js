adeefa413697ae40e6411fcbd3337087d98126c5
Kuzmin Anton <akuzmin.exceedteam@gmail.com>
2021-10-04 12:24:54 +0300

    BE 4 - card (login)

--- src/modules/routes/routes.js
+++ src/modules/routes/routes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 
 const {
   newUser,
+  login,
 } = require('../controllers/user.cotroller');
 const {} = require('../controllers/appointment.controller');
 
 router.post('/auth/newUser', newUser);
+router.post('/auth/login', login);
 
 module.exports = router;
