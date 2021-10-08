9a1437df49e73cb9659b32841f4c0e805ff3d3f9
Kuzmin Anton <akuzmin.exceedteam@gmail.com>
2021-10-08 16:49:37 +0300

    BE 8 - card (delete appointment)

--- src/modules/routes/routes.js
+++ src/modules/routes/routes.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const auth = require('../../middleware/auth.middleware');
 
 const router = express.Router();
 
-const {} = require('../controllers/user.cotroller');
-const {} = require('../controllers/appointment.controller');
+const {
+  newUser,
+  login,
+} = require('../controllers/user.cotroller');
+const {
+  addAppointments,
+  getAppointments,
+  getDoctors,
+  editAppointments,
+  deleteAppointments,
+} = require('../controllers/appointment.controller');
+
+router.post('/auth/newUser', newUser);
+router.post('/auth/login', login);
+router.post('/getDoctors', auth, getDoctors);
+router.post('/addAppointments', auth, addAppointments);
+router.post('/getAppointments', auth, getAppointments);
+router.patch('/editAppointments', auth, editAppointments);
+router.delete('/deleteAppointments', auth, deleteAppointments);
 
 module.exports = router;
