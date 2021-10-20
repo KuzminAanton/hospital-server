11ed9f48f924e4a6bc0f503abdaab47c3a6d6296
Kuzmin Anton <akuzmin.exceedteam@gmail.com>
2021-10-20 15:31:47 +0300

    BE 6 - card (add appointment)

--- src/modules/routes/routes.js
+++ src/modules/routes/routes.js
@@ -9,10 +9,14 @@ const {
 } = require('../controllers/user.cotroller');
 const {
   getAppointments,
+  addAppointments,
+  getDoctors,
 } = require('../controllers/appointment.controller');
 
 router.post('/auth/newUser', newUser);
 router.post('/auth/login', login);
 router.get('/getAppointments', auth, getAppointments);
+router.post('/addAppointments', auth, addAppointments);
+router.get('/getDoctors', auth, getDoctors);
 
 module.exports = router;
